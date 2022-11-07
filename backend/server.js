391bc928d05f53f100218e1cee53be88be6cffb8
mangala-karunarathne <mangalaicc@gmail.com>
2022-11-07 22:33:36 +0530

    CHG | fetch data from product api

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import data from './data.js';
+import cors from 'cors';
 
 const app = express();
+// const cors = require('cors');
+
 
 app.get('/', (req,res) => {
     res.send('Server is ready');
 });
 
+
+app.use(cors());
+// app.use(cors({ origin: true, credentials: true }));
+
 app.get('/api/products', (req,res) => {
+    console.log("first", data)
     res.send(data.products);
 })
 

b2ed08a66767c40d659bed82709ea0d0f30ef663
mangala-karunarathne <mangalaicc@gmail.com>
2022-11-07 12:12:33 +0530

    CHG | products api with simple changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import data from './data.js';
 
 const app = express();
 
@@ -6,6 +7,12 @@ app.get('/', (req,res) => {
     res.send('Server is ready');
 });
 
-app.listen(5000, () => {
-    console.log('Server at http://localhost:5000')
+app.get('/api/products', (req,res) => {
+    res.send(data.products);
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`)
 })
